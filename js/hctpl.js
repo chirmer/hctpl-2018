e00e0e35b10d6a3e0f192e55a31d4155471d5e1a
Devon Henderson (chirmer) <dvnhndrsn@gmail.com>
2017-11-06 16:58:06 -0500

    Add jQuery for menu, search buttons.

--- js/hctpl.js
+++ js/hctpl.js
@@ -1,16 +1,10 @@
-// Main menu show & hide
-
-function menuMainShow() {
-    var x = document.getElementById("menu-main");
-    var y = document.getElementById("btn-menu-main");
-    if (x.style.display === "none") {
-        x.style.display = "flex";
-    } else {
-        x.style.display = "none";
-    }
-    if (x.style.display === "flex") {
-        y.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
-    } else {
-        y.style.backgroundColor = "#3155a4";
-    }
-}
+$(document).ready(function () {
+  // Main menu show & hide
+  $("#btn-menu-main").click(function() {
+    $("#menu-main").toggleClass("hidden");
+  });
+  // Search show & hide
+  $("#btn-search-box").click(function() {
+    $("#search-box").toggleClass("hidden");
+  });
+});
