06e3d9a8ea47a84d56718e97451928c55402c05f
Devon Henderson (chirmer) <dvnhndrsn@gmail.com>
2017-11-07 10:30:57 -0500

    Finish page template.

--- js/hctpl.js
+++ js/hctpl.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
   // Main menu show & hide
   $("#btn-menu-main").click(function() {
     $("#menu-main").toggleClass("hidden");
+    $("#btn-menu-main").toggleClass("bg-dark");
   });
   // Search show & hide
   $("#btn-search-box").click(function() {
     $("#search-box").toggleClass("hidden");
+    $("#btn-search-box").toggleClass("bg-dark");
   });
 });
