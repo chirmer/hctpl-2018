5cbda6853cb27e5c853c197999b5250aff4a1ce5
Devon Henderson (chirmer) <dvnhndrsn@gmail.com>
2017-11-07 10:56:06 -0500

    Test jQuery catalog search.

--- js/hctpl.js
+++ js/hctpl.js
@@ -9,4 +9,10 @@ $(document).ready(function () {
     $("#search-box").toggleClass("hidden");
     $("#btn-search-box").toggleClass("bg-dark");
   });
+  //Search Catalog
+  $('#btn-search-catalog').click(function() {
+    var searchInputA = $('#input-search').val();
+    var url = "http://hctp.ent.sirsi.net/client/en_US/default/search/results?qu=" + searchInputA + "&te=ILS";
+    window.open(url);
+  });
 });
