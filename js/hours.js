c13765d38aa53420dd3632b12e785703298725dc
Devon Henderson (chirmer) <dvnhndrsn@gmail.com>
2017-11-09 10:43:36 -0500

    Add hours, social to header.

--- js/hours.js
+++ js/hours.js
@@ -1,21 +1,21 @@
 var dayOfWeek = (new Date).getDay();
 var hours = ["Huntington & Markle are closed today.",          // Sunday
-             "<b>Huntington: </b>  9 a.m. - 8 p.m.",   // Monday
-             "<b>Huntington: </b>  9 a.m. - 8 p.m.",   // Tuesday
-             "<b>Huntington: </b>  9 a.m. - 8 p.m.",   // Wednesday
-             "<b>Huntington: </b>  9 a.m. - 8 p.m.",   // Thursday
-             "<b>Huntington: </b>  9 a.m. - 5 p.m.",   // Friday
-             "<b>Huntington: </b>  9 a.m. - 5 p.m."];  // Saturday
+             "Huntington:   9 a.m. - 8 p.m.",   // Monday
+             "Huntington:   9 a.m. - 8 p.m.",   // Tuesday
+             "Huntington:   9 a.m. - 8 p.m.",   // Wednesday
+             "Huntington:   9 a.m. - 8 p.m.",   // Thursday
+             "Huntington:   9 a.m. - 5 p.m.",   // Friday
+             "Huntington:   9 a.m. - 5 p.m."];  // Saturday
 var todaysHours = hours[dayOfWeek];
 document.getElementById("hhours").innerHTML = todaysHours;
 
 var dayOfWeek = (new Date).getDay();
 var hours = ["",          // Sunday
-             "<b>Markle: </b>  12 p.m. - 7 p.m.",   // Monday
-             "<b>Markle: </b>  10 a.m. - 5 p.m.",   // Tuesday
-             "<b>Markle: </b>  12 p.m. - 7 p.m.",   // Wednesday
-             "<b>Markle: </b>  10 a.m. - 5 p.m.",   // Thursday
-             "<b>Markle: </b>  12 p.m. - 7 p.m.",   // Friday
-             "<b>Markle: </b>  10 a.m. - 5 p.m."];  // Saturday
+             "Markle:   12 p.m. - 7 p.m.",   // Monday
+             "Markle:   10 a.m. - 5 p.m.",   // Tuesday
+             "Markle:   12 p.m. - 7 p.m.",   // Wednesday
+             "Markle:   10 a.m. - 5 p.m.",   // Thursday
+             "Markle:   12 p.m. - 7 p.m.",   // Friday
+             "Markle:   10 a.m. - 5 p.m."];  // Saturday
 var todaysHours = hours[dayOfWeek];
 document.getElementById("mhours").innerHTML = todaysHours;
