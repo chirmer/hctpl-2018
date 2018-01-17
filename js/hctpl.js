3ff47eb394f28683b9249e69d39ce5d7ec11c5f6
Devon Henderson (chirmer) <dvnhndrsn@gmail.com>
2018-01-17 10:36:59 -0500

    Update header, footer on Calendar page.

--- js/hctpl.js
+++ js/hctpl.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
   //Search Website
   $('#btn-search-website').click(function() {
     var searchInputA = $('#input-search').val();
-    var url = "https://www.google.com/search?&q=site+huntingtonpub.lib.in.us+" + searchInputA;
+    var url = "https://www.google.com/search?&q=site%3Ahuntingtonpub.lib.in.us+" + searchInputA;
     window.open(url);
   });
   // Site home link
