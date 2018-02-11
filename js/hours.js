91f7d6581c7943a6961c956ae1407ec5d0dc1d87
Devon <dvnhndrsn@gmail.com>
2018-02-11 16:15:53 -0500

    Fix page template media queries.

--- js/hours.js
+++ js/hours.js
@@ -1,23 +1,23 @@
 var dayOfWeek = (new Date).getDay();
-var hours = ["Huntington & Markle are closed today.",          // Sunday
-             "Huntington:   9 a.m. - 8 p.m.",   // Monday
-             "Huntington:   9 a.m. - 8 p.m.",   // Tuesday
-             "Huntington:   9 a.m. - 8 p.m.",   // Wednesday
-             "Huntington:   9 a.m. - 8 p.m.",   // Thursday
-             "Huntington:   9 a.m. - 5 p.m.",   // Friday
-             "Huntington:   9 a.m. - 5 p.m."];  // Saturday
+var hours = ["Closed today; opens 9 a.m. Monday.",          // Sunday
+             "Open 9 a.m. - 8 p.m.",   // Monday
+             "Open 9 a.m. - 8 p.m.",   // Tuesday
+             "Open 9 a.m. - 8 p.m.",   // Wednesday
+             "Open 9 a.m. - 8 p.m.",   // Thursday
+             "Open 9 a.m. - 5 p.m.",   // Friday
+             "Open 9 a.m. - 5 p.m."];  // Saturday
 var todaysHours = hours[dayOfWeek];
 document.getElementById("hhours").innerHTML = todaysHours;
 document.getElementById("hhours-m").innerHTML = todaysHours;
 
 var dayOfWeek = (new Date).getDay();
-var hours = ["",          // Sunday
-             "Markle:   12 p.m. - 7 p.m.",   // Monday
-             "Markle:   10 a.m. - 5 p.m.",   // Tuesday
-             "Markle:   12 p.m. - 7 p.m.",   // Wednesday
-             "Markle:   10 a.m. - 5 p.m.",   // Thursday
-             "Markle:   12 p.m. - 7 p.m.",   // Friday
-             "Markle:   10 a.m. - 5 p.m."];  // Saturday
+var hours = ["Closed today; opens noon Monday.",          // Sunday
+             "Open noon - 7 p.m.",   // Monday
+             "Open 10 a.m. - 5 p.m.",   // Tuesday
+             "Open noon - 7 p.m.",   // Wednesday
+             "Open 10 a.m. - 5 p.m.",   // Thursday
+             "Open noon - 7 p.m.",   // Friday
+             "Open 10 a.m. - 5 p.m."];  // Saturday
 var todaysHours = hours[dayOfWeek];
 document.getElementById("mhours").innerHTML = todaysHours;
 document.getElementById("mhours-m").innerHTML = todaysHours;
